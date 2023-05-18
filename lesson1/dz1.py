6bc8194ab44f569a588d84633843ad63bbb0bfeb
nesonzahartms <nesonzahar@gmail.com>
2023-05-18 16:21:23 +0300

    lesson1

---
+++ lesson1/dz1.py
@@ -0,0 +1,38 @@
+1.
+print(2+3)
+print(2-3)
+print(2*3)
+print(2**3)
+print(2%3)
+print(2/3)
+print(2//3)
+2.
+x = 17/2*3+2
+print(x)
+x = ((17/2)*3)+2
+print(x)
+x = 2+17/2*3
+print(x)
+x  = 2+((17/2)*3)
+print(x)
+x = 19%4+15/2*3
+print(x)
+x = (19%4) + ((15 / 2) * 3)
+print(x)
+x = (15+6)-10*4
+print(x)
+x = (15+6)-(10*4)
+print(x)
+x = ((17/2)%2)*(3**3)
+print(x)
+x = 17/2%2*3**3
+print(x)
+3.
+x = 4
+y = 3
+z = (abs(x)-y)/(1+abs(x*y))
+print(z)
+
+
+string = "aaaaaBccBddBeeBffBggB"
+result = string5[::3]
---
+++ lesson2/dz2.py
@@ -0,0 +1,33 @@
+one = two = three = 8 # int
+print(id(one))
+print(id(two))
+print(id(three))
+print(one, two, three)
+
+four, five = 4, 'Hello' #int and str
+print(id(four))
+print(id(five))
+
+a = 'replacement1'
+print(a)
+print(id(float(one)))
+print(id(str(two)))
+print(id(bool(three)))
+b = 'replacement2'
+print(b)
+
+print(id(bool(4)))
+print(id(bool('Hello')))
+print('next exercise')
+
+line = input()
+print(line)
+variable_one = 246
+variable_two = 135
+
+
+
+
+
+
+
